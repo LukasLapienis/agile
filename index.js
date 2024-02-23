aa6b6ce717fcdd4483d1bc790eeed678ad9d2a28
LukasLapienis <lapienislukasp@gmail.com>
2024-02-23 08:48:44 +0200

    pradejau

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const searchButton = document.getElementById('searchButton')
+
+searchButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    const searchInput = document.getElementById('searchInput')
+    const textInput = document.getElementById('textInput')
+
+
+    const displayContainer = document.getElementById('displayContainer')
+    displayContainer.innerText = textInput.value
+})
